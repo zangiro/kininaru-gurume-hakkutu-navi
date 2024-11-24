8e16507f0baf41efdfe3d8ec78e4d7ad1375f9fa
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2024-11-24 18:30:04 +0900

    gem rspecインストール＋rails gのhelper設定追加

--- Gemfile
+++ Gemfile
@@ -60,6 +60,10 @@ group :development, :test do
   gem "rubocop-rails", require: false
   gem "rubocop-performance", require: false
   gem "rubocop-rspec", require: false
+
+  gem "rspec-rails"
+  gem "factory_bot_rails"
+  gem "faker"
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module Myapp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.generators do |g|
+      g.helper false
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
