0efe333d510dd5691be14137e945b770c367ad6a
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2024-12-04 03:38:17 +0900

    マイページ、アカウント情報ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login, only: %i[index]
+  before_action :require_login, only: %i[show edit update]
 
   def new
     @user = User.new
@@ -15,6 +15,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
+    unless params[:user][:agree_terms] == "1"
+      render :new and return
+    end
+
     if @user.save
       redirect_to root_path
     else
@@ -22,9 +27,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :is_public)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :is_public, :introduction)
   end
 end
