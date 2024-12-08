7e9807a9df58f65e7180a207729c4e1eec7efde2
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2024-12-09 00:25:08 +0900

    gem importmap-railsインストール完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
 
   belongs_to :user
-  has_one :dish
+  has_one :dish, dependent: :destroy
   accepts_nested_attributes_for :dish
 end
