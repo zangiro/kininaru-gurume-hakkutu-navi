f907f4904ea2271a350fbfc8f6baac49398bf5f2
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-01-24 13:24:57 +0900

    Turbo Streamsでいいねボタン押したとき対応ファイル呼び出し成功（設定は仮）

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,17 +7,13 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @user = params[:user_id] ? User.find(params[:user_id]) : []
-    @post_path = params[:post_path]
-    current_user.like(@post)   # userモデルのlikeメソッド
-    redirect_to post_path(@post, user_id: @user.id, post_path: @post_path), success: "@いいねに登録"
+    #current_user.like(@post)   # userモデルのlikeメソッド
+    # リダイレクトしない際create.turbo_stream.erbが呼ばれる
   end
 
   def destroy
     @post = current_user.likes.find_by(post_id: params[:post_id]).post
-    @user = params[:user_id] ? User.find(params[:user_id]) : []
-    @post_path = params[:post_path]
-    current_user.unlike(@post)   # userモデルのunlikeメソッド
-    redirect_to post_path(@post, user_id: @user.id, post_path: @post_path), success: "@いいねを解除"
+    #current_user.unlike(@post)   # userモデルのunlikeメソッド
+    # リダイレクトしない際destroy.turbo_stream.erbが呼ばれる
   end
 end
