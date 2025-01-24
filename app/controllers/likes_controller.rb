c85c157f1213bddbad00d333fed812761843c0ec
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-01-24 17:25:51 +0900

    Turbo Streamsの中からボタン呼び出しテスト中（エラーなし状態でチェックポイント）

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,12 +8,17 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.like(@post)   # userモデルのlikeメソッド
+
     # リダイレクトしない際create.turbo_stream.erbが呼ばれる
   end
 
   def destroy
     @post = current_user.likes.find_by(post_id: params[:post_id]).post
+    #@post = current_user.likes.find(params[:post_id]).post
+
+    #board = current_user.bookmarks.find(params[:id]).board
     current_user.unlike(@post)   # userモデルのunlikeメソッド
+
     # リダイレクトしない際destroy.turbo_stream.erbが呼ばれる
   end
 end
