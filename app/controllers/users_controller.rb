7402927881f1513fe6176631da5e5a1e5889196f
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-02-01 15:22:48 +0900

    アバターの登録OK。ヘッダーにアバター表示OK。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :is_public, :introduction, :agree_terms)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :is_public, :introduction, :agree_terms, :avatar)
   end
 end
