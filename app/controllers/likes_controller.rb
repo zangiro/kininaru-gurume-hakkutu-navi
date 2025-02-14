400342913813001c60fd41246756f1281f078eef
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-02-14 19:04:34 +0900

    いいねのturbo_stream完成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,18 +8,13 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.like(@post)   # userモデルのlikeメソッド
-
+    @like = @post.likes.find_by(user_id: current_user.id)
     # リダイレクトしない際create.turbo_stream.erbが呼ばれる
   end
 
   def destroy
     @post = current_user.likes.find_by(post_id: params[:post_id]).post
-    # @post = current_user.likes.find(params[:post_id]).post
-    # board = current_user.bookmarks.find(params[:id]).board
-    # @likes = @post.likes
-    # @like = @likes.find_by(user_id: @user.id)
     current_user.unlike(@post)   # userモデルのunlikeメソッド
-
     # リダイレクトしない際destroy.turbo_stream.erbが呼ばれる
   end
 end
