0bd993dbc21141f1af570e9aef66be0248616c28
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-02-19 15:54:52 +0900

    lint修正

--- app/controllers/view_histories_controller.rb
+++ app/controllers/view_histories_controller.rb
@@ -2,7 +2,7 @@ class ViewHistoriesController < ApplicationController
   before_action :require_login, only: %i[index]
 
   def index
-    @view_history_posts = current_user.view_history_posts.order('view_histories.created_at DESC').page(params[:page]).per(2)
+    @view_history_posts = current_user.view_history_posts.order("view_histories.created_at DESC").page(params[:page]).per(2)
     @user = current_user
     @post_path = "5"
   end
--- spec/factories/view_histories.rb
+++ spec/factories/view_histories.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :view_history do
-    
   end
 end
