7a05e891ede334c215fe5adc71cf7e39db6dbd41
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-02-25 07:42:50 +0900

    トップページのおすすめ表示。非ログインor閲覧履歴なし状態の表示に対応

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,10 @@ class StaticPagesController < ApplicationController
       # この処理で@area_tagsのAreaTagの情報のnameカラムで["word1","word2","word3"]という配列を作る
       @recommendations = Post.joins(:area_tags).where(area_tags: { name: @words }).where.not(user_id: current_user.id).distinct.limit(8)
     else
-      @recommendations = Post.all
+      @all_user_view_histories = Post.joins(:view_histories).order("view_histories.created_at DESC").limit(30)
+      @area_tags = AreaTag.joins(:posts).where(posts: { id: @all_user_view_histories }).group("area_tags.id").order("COUNT(area_tags.id) DESC").limit(5)
+      @words = @area_tags.map(&:name)
+      @recommendations = Post.joins(:area_tags).where(area_tags: { name: @words }).distinct.limit(8)
     end
   end
 end
