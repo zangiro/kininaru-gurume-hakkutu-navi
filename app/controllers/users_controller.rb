964b7b662f9ace0f8096c5818293c6aea1c6d943
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-02-28 21:31:09 +0900

    ユーザー登録後の自動ログインok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to root_path, success: "@ユーザーの新規作成をしました"
     else
       flash.now[:danger] = "@失敗しました"
