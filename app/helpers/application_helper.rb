da61a9015fa1f390d92727f0dcc393b04f974bfe
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-02-28 10:34:12 +0900

    動的タイトル表示。動作確認OK。後は各ビューに初期値設定するだけ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def page_title(title = "")   # メソッドが呼ばれたときに引数が渡されなかった場合に、デフォルトで空の文字列を使う意味
+    @base_title = "@気になるグルメ発掘ナビ"
+    title.present? ? "#{title} | #{@base_title}" : @base_title
+  end
 end
