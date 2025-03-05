5f37f1d4b732227d0432812c65c1ae0b3a86cf98
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-03-05 11:41:49 +0900

    turbo_stream使ったタグ機能、area_tagの部分完成

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,13 @@
 class TagsController < ApplicationController
   def index
-    @area_tags = AreaTag.joins(:posts).group(:name).select("name, COUNT(*) as count").limit(3)
+    @area_tags = AreaTag.joins(:posts).group(:name).select("name, COUNT(*) as count").order("count DESC").limit(3)
     @genre_tags = GenreTag.joins(:posts).group(:name).select("name, COUNT(*) as count")
     @taste_tags = TasteTag.joins(:posts).group(:name).select("name, COUNT(*) as count")
     @outher_tags = OutherTag.joins(:posts).group(:name).select("name, COUNT(*) as count")
   end
 
   def replace_area_tags
-    @area_tags = AreaTag.joins(:posts).group(:name).select("name, COUNT(*) as count").limit(12)
-    render "tags/replace_area_tags", content_type: 'text/vnd.turbo-stream.html'
+    @area_tags = AreaTag.joins(:posts).group(:name).select("name, COUNT(*) as count").order("count DESC")
+    render "tags/replace_area_tags", content_type: "text/vnd.turbo-stream.html"
   end
 end
