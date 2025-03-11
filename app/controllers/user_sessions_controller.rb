54ffd66f82659f993f4e98df860a6c2f193e323b
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-03-11 11:25:02 +0900

    rails-i18n_post,user_session,view_historiesコントローラー

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,16 +6,16 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to root_path, success: "@ログインしました"
+      redirect_to root_path, success: t("flash_message.login_success")
     else
-      flash.now[:danger] = "@ログインに失敗しました"
+      flash.now[:danger] = t("flash_message.login_failure")
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    flash[:success] = "@ログアウトしました"
+    flash[:success] = t("flash_message.logout")
     redirect_to root_path, status: :see_other
   end
 end
