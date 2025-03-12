d7523617b8b57961ef1375874b47e7b0fe7d113b
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-03-12 21:00:38 +0900

    ogp_OK

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,31 @@ module ApplicationHelper
     @base_title = "@気になるグルメ発掘ナビ"
     title.present? ? "#{title} | #{@base_title}" : @base_title
   end
+
+  def default_meta_tags
+    {
+      site: "気になるグルメ発掘ナビ",
+      title: "画像でおいしそうなグルメ情報の発掘をサポートするアプリ",
+      reverse: true,
+      charset: "utf-8",
+      description: "今なにを食べたいかは二の次。まずはどんな美味しいグルメが私達の周りにあるかから始めませんか？お手軽操作で幅広いグルメ情報収集を目的としたアプリです。",
+      keywords: "料理,グルメ,見た目,発掘,画像",
+      canonical: "https://myapp-young-night-9527.fly.dev/",
+      separator: "|",
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: "website",
+        url: "https://myapp-young-night-9527.fly.dev/",
+        image: image_url("ogp.png"),
+        local: "ja-JP"
+      },
+      twitter: {
+        card: "summary",
+        site: "@bfkjs8",
+        image: image_url("ogp.png")
+      }
+    }
+  end
 end
