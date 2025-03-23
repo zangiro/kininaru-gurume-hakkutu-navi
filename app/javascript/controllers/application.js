504dbbdcf02241d782983d112923e18039f0a448
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-03-23 17:30:03 +0900

    app/javascript/controllers/applicationに設定記述

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,11 @@
 import { Application } from "@hotwired/stimulus"
+import { Autocomplete } from "stimulus-autocomplete"
+// コンポーネントを読み込むための記述
 
 const application = Application.start()
+application.register("autocomplete", Autocomplete) 
+// コンポーネントにあるAutocompleteコントローラを使えるようにするための記述
+// HTML内でdata-controller="autocomplete"の属性をもつ要素に対して、Autocompleteコントローラの機能が適用される
 
 // Configure Stimulus development experience
 application.debug = false
