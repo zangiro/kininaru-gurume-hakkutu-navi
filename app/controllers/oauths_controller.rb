b2d15006083ec3fb82fded06ce5d642583a81741
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-03-30 16:27:18 +0900

    callbackメソッドでログイン時バリデーション設定？でエラー

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,16 +8,13 @@ class OauthsController < ApplicationController
     provider = auth_params[:provider]
     # 既存のユーザーをプロバイダ情報を元に検索し、存在すればログイン
     if (@user = login_from(provider))
-      flash.now[:danger] = "0"
-      redirect_to root_path
+      redirect_to searchs_path
     else
       begin
         # ユーザーが存在しない場合はプロバイダ情報を元に新規ユーザーを作成し、ログイン
         signup_and_login(provider)
-        flash.now[:danger] = "1"
-        redirect_to root_path
+        redirect_to tags_path
       rescue
-        flash.now[:danger] = "2"
         redirect_to root_path
       end
     end
@@ -26,8 +23,7 @@ class OauthsController < ApplicationController
   private
 
   def auth_params
-    params.permit(:code, :provider)
-    # params.permit(:code, :provider, :scope, :authuser, :prompt)
+    params.permit(:code, :provider, :scope, :authuser, :prompt)
   end
 
   def signup_and_login(provider)
