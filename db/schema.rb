a431a69756b216fcbeaef158f5e65a4d33da28e1
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-04-16 21:31:43 +0900

    住所緯度経度の新しいカラム追加+db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_30_090918) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_16_121833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -179,6 +179,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_30_090918) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
+    t.text "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
