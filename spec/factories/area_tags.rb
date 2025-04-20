63b03a81652d8207a2a8cd8258c8714c26034282
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-04-20 16:38:34 +0900

    area_tagのモデルスペックok

--- spec/factories/area_tags.rb
+++ spec/factories/area_tags.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
   factory :area_tag do
+    name { "エリアA" }
   end
 end
--- spec/models/area_tag_spec.rb
+++ spec/models/area_tag_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe AreaTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'バリデーションチェック' do
+    it 'area_tagモデルに定義したバリデーションが全て機能しているか' do
+      area_tag = build(:area_tag)
+      expect(area_tag).to be_valid
+      expect(area_tag.errors).to be_empty
+    end
+
+    it 'エリア名は必須項目であること' do
+      area_tag = build(:area_tag, name: nil)
+      area_tag.valid?
+      expect(area_tag.errors[:name]).to include('を入力してください')
+    end
+  end
 end
