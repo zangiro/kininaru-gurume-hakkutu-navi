546ac92b00b21fdcefcbec5a37583cc992b160b9
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-04-21 13:59:39 +0900

    postのバリデーション

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,6 +7,26 @@ RSpec.describe Post, type: :model do
       expect(post).to be_valid
       expect(post.errors).to be_empty
     end
+
+    it 'タイトルは必須項目であること' do
+      post = build(:post, title: nil)
+      post.valid?
+      expect(post.errors[:title]).to include('を入力してください')
+    end
+
+    it 'メイン画像添付が必須項目であること' do
+      user = create(:user)
+      post = Post.new(title: "記事A", user: user)
+      post.valid?
+      expect(post.errors[:main_image]).to include('を添付してください')
+    end
+    # build(:post, main_image: nil)では作成できないのでテストケース内でPost作成
+
+    it 'タイトルは255文字以下であること' do
+      post = build(:post, title: 'a' * 256)
+      post.valid?
+      expect(post.errors[:title]).to include('は255文字以内で入力してください')
+    end
   end
 
   # 複数のタグを持てること
