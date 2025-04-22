958f89848402aafc6e67ec125b78ff437aae415d
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-04-22 20:55:08 +0900

    システムスペック、ログイン処理が成功する、ok

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
+  include LoginMacros
+
   before do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe "ログイン前" do
+    context "フォームの入力値が正常" do
+      it "ログイン処理が成功する" do
+        visit login_path
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+        expect(page).to have_content "ログインしました"
+        expect(current_path).to eq root_path
+      end
+    end
+
+  end
 end
