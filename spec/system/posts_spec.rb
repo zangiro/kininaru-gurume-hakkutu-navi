e0b3cb9426b0fe75d6e333c6946a1239b0f31bef
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-04-24 15:34:30 +0900

    記事の作成が失敗するテストケース、タグ

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -70,7 +70,45 @@ RSpec.describe "Posts", type: :system do
           expect(page).to have_content "記事の作成に失敗しました"
           expect(page).to have_content "メイン画像を添付してください"
           expect(current_path).to eq posts_path
-          # render :new実行後はnew_post_pathではなくposts_path
+        end
+      end
+
+      context "詳細用紹介文が未入力" do
+        it "記事の作成が失敗する" do
+          visit new_post_path
+          fill_in "料理名", with: "ごはん"
+          attach_file "メイン画像", Rails.root.join("spec/fixtures/files/1.jpg")
+          fill_in "出典", with: "出典A"
+          fill_in "店舗情報", with: "店A"
+          fill_in "一覧用紹介文", with: "いちらんせつめい"
+          fill_in "詳細用紹介文", with: ""
+          fill_in "post[post_area_tags_attributes][0][area_tag_attributes][name]", with: "エリアA,エリアB"
+          fill_in "post[post_genre_tags_attributes][0][genre_tag_attributes][name]", with: "ジャンルA,ジャンルB"
+          fill_in "post[post_taste_tags_attributes][0][taste_tag_attributes][name]", with: "味A,味B"
+          fill_in "post[post_outher_tags_attributes][0][outher_tag_attributes][name]", with: "その他A,その他B"
+          click_button "登録"
+          expect(page).to have_content "記事の作成に失敗しました"
+          expect(page).to have_content "詳細用紹介文を入力してください"
+          expect(current_path).to eq posts_path
+        end
+      end
+
+      context "エリアタグが未入力" do
+        it "記事の作成が失敗する" do
+          visit new_post_path
+          fill_in "料理名", with: "ごはん"
+          attach_file "メイン画像", Rails.root.join("spec/fixtures/files/1.jpg")
+          fill_in "出典", with: "出典A"
+          fill_in "店舗情報", with: "店A"
+          fill_in "一覧用紹介文", with: "いちらんせつめい"
+          fill_in "詳細用紹介文", with: "しょうさいせつめい"
+          fill_in "post[post_area_tags_attributes][0][area_tag_attributes][name]", with: ""
+          fill_in "post[post_genre_tags_attributes][0][genre_tag_attributes][name]", with: "ジャンルA,ジャンルB"
+          fill_in "post[post_taste_tags_attributes][0][taste_tag_attributes][name]", with: "味A,味B"
+          fill_in "post[post_outher_tags_attributes][0][outher_tag_attributes][name]", with: "その他A,その他B"
+          click_button "登録"
+          expect(page).to have_content "タグが入力されてません"
+          expect(current_path).to eq posts_path
         end
       end
     end
