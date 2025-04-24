7932f6ed7a286fa90a5459186a20c1ef25438b7a
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-04-24 19:29:38 +0900

    システムスペック概ね完了

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -8,6 +8,38 @@ RSpec.describe "Posts", type: :system do
   end
 
   let(:user) { create(:user) }
+  let(:dish) { create(:dish) }
+  let(:area_tag) { create(:area_tag) }
+  let(:genre_tag) { create(:genre_tag) }
+  let(:taste_tag) { create(:taste_tag) }
+  let(:outher_tag) { create(:outher_tag) }
+  let(:post) { create(:post, user: user, dish: dish, area_tags: [area_tag], genre_tags: [genre_tag], taste_tags: [taste_tag], outher_tags: [outher_tag]) }
+
+  describe "ログイン前" do
+    describe "ページ遷移確認" do
+      context "記事作成ページにアクセス" do
+        it "記事作成ページへのアクセスが失敗する" do
+          visit new_post_path
+          expect(current_path).to eq login_path
+        end
+      end
+
+      context "記事編集ページにアクセス" do
+        it "記事編集ページへのアクセスが失敗する" do
+          visit edit_post_path(post)
+          expect(current_path).to eq login_path
+        end
+      end
+
+      context "記事詳細ページにアクセス" do
+        it "記事詳細ページへのアクセスが成功する" do
+          visit post_path(post)
+          expect(page).to have_content post.title
+          expect(current_path).to eq post_path(post)
+        end
+      end
+    end
+  end
 
   describe "ログイン後" do
     before { login_as(user) }
@@ -124,12 +156,6 @@ RSpec.describe "Posts", type: :system do
     end
 
     describe "記事編集" do
-      let(:dish) { create(:dish) }
-      let(:area_tag) { create(:area_tag) }
-      let(:genre_tag) { create(:genre_tag) }
-      let(:taste_tag) { create(:taste_tag) }
-      let(:outher_tag) { create(:outher_tag) }
-      let(:post) { create(:post, user: user, dish: dish, area_tags: [area_tag], genre_tags: [genre_tag], taste_tags: [taste_tag], outher_tags: [outher_tag]) }
       context "フォームの入力値が正常" do
         it "記事の編集が成功する" do
           visit edit_post_path(post)
@@ -149,41 +175,6 @@ RSpec.describe "Posts", type: :system do
           click_button "更新"
           expect(page).to have_content "更新しました"
           expect(current_path).to eq user_posts_path(user)
-        end
-      end
-
-      context "料理名が未入力" do
-        xit "記事の編集が失敗する" do
-          visit edit_post_path(post)
-          fill_in "料理名", with: ""
-          attach_file "メイン画像", Rails.root.join("spec/fixtures/files/susi.jpg")
-          attach_file "サブ画像１", Rails.root.join("spec/fixtures/files/susi.jpg")
-          attach_file "サブ画像２", Rails.root.join("spec/fixtures/files/susi.jpg")
-          fill_in "出典", with: "出典B"
-          fill_in "店舗情報", with: "店B"
-          fill_in "一覧用紹介文", with: "いちらんせつめいB"
-          fill_in "詳細用紹介文", with: "しょうさいせつめいB"
-          fill_in "post[post_area_tags_attributes][0][area_tag_attributes][name]", with: "エリアA,エリアB"
-          fill_in "post[post_genre_tags_attributes][0][genre_tag_attributes][name]", with: "ジャンルA,ジャンルB"
-          fill_in "post[post_taste_tags_attributes][0][taste_tag_attributes][name]", with: "味A,味B"
-          fill_in "post[post_outher_tags_attributes][0][outher_tag_attributes][name]", with: "その他A,その他B"
-          # text_field_tagを使っている場合は、fill_inで指定する名前も実際の名前に合わせる必要がある
-          click_button "更新"
-          expect(page).to have_content "更新失敗しました"
-          expect(page).to have_content "料理名を入力してください"
-          expect(current_path).to eq posts_path
-        end
-      end
-
-      context "料理名が未入力" do
-        xit "記事の編集が失敗する" do
-          visit edit_post_path(post)
-          fill_in "料理名", with: ""
-          attach_file "メイン画像", Rails.root.join("spec/fixtures/files/susi.jpg")
-          click_button "更新"
-          expect(page).to have_content "更新失敗しました"
-          expect(page).to have_content "料理名を入力してください"
-          expect(current_path).to eq posts_path
         end
       end
     end
