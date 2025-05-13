5b4dc08a775c5283631cd647c7b29e84216ddeb5
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-05-13 14:25:03 +0900

    トップページ説明文ok

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,14 +6,6 @@ class StaticPagesController < ApplicationController
       @words = @area_tags.map(&:name)
       # この処理で@area_tagsのAreaTagの情報のnameカラムで["word1","word2","word3"]という配列を作る
       @recommendations = Post.joins(:area_tags).where(area_tags: { name: @words }).where.not(user_id: current_user.id).distinct.limit(8)
-
-
-
-
-
-
-
-      
     else
       @all_user_view_histories = Post.joins(:view_histories).order("view_histories.created_at DESC").limit(30)
       @area_tags = AreaTag.joins(:posts).where(posts: { id: @all_user_view_histories }).group("area_tags.id").order("COUNT(area_tags.id) DESC").limit(5)
