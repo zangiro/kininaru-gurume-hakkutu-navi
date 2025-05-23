762f573ecc1fa199136ce45669358a4ace8adc1a
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-05-23 17:57:12 +0900

    admin/user/editで変更できるようする

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,9 +3,27 @@ class Admin::UsersController < ApplicationController
   before_action :admin_user
 
   def index
-    @users = User.all
+    @users = User.order(:id).page(params[:page]).per(10)
   end
 
-  def show
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_path, success: t("flash_message.update_success")
+    else
+      flash.now[:danger] = t("flash_message.update_failure")
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
