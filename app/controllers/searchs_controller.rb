bb12902d5c6f6dd694b56c0cb4bcabce706e4266
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-05-25 20:07:11 +0900

    停止中の記事非表示3

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -5,10 +5,22 @@ class SearchsController < ApplicationController
     @taste_tags = params[:taste_tags] || []
     @outher_tags = params[:outher_tags] || []
 
-    @area_posts = Post.joins(:area_tags).where(area_tags: { name: @area_tags })
-    @genre_posts = Post.joins(:genre_tags).where(genre_tags: { name: @genre_tags })
-    @taste_posts = Post.joins(:taste_tags).where(taste_tags: { name: @taste_tags })
-    @outher_posts = Post.joins(:outher_tags).where(outher_tags: { name: @outher_tags })
+    @area_posts = Post.joins(:user)
+                      .joins(:area_tags)
+                      .where(area_tags: { name: @area_tags })
+                      .where.not(users: { account_status: 1 })
+    @genre_posts = Post.joins(:user)
+                       .joins(:genre_tags)
+                       .where(genre_tags: { name: @genre_tags })
+                       .where.not(users: { account_status: 1 })
+    @taste_posts = Post.joins(:user)
+                       .joins(:taste_tags)
+                       .where(taste_tags: { name: @taste_tags })
+                       .where.not(users: { account_status: 1 })
+    @outher_posts = Post.joins(:user)
+                        .joins(:outher_tags)
+                        .where(outher_tags: { name: @outher_tags })
+                        .where.not(users: { account_status: 1 })
 
     @post_ids = @area_posts.pluck(:id) + @genre_posts.pluck(:id) + @taste_posts.pluck(:id) + @outher_posts.pluck(:id)
 
@@ -27,15 +39,28 @@ class SearchsController < ApplicationController
     @maximum_number = Post.how_many_posts?(@search_posts.count)
     # how_many_posts?は@search_postsの数に応じて変数に数字を代入する。
     # おすすめ表示で使用。ヒット数が少なければおおっきい数字を。多ければ0~少ない数字を返す。
-
+    
     if logged_in?
-      @all_users_view_histories = ViewHistory.where.not(user_id: current_user.id).order(created_at: :desc).limit(50)
+      @all_users_view_histories = ViewHistory.joins(:user)
+                                             .where.not(user_id: current_user.id)
+                                             .where.not(users: { account_status: 1 })
+                                             .order(created_at: :desc)
+                                             .limit(50)
     else
-      @all_users_view_histories = ViewHistory.order(created_at: :desc).limit(50)
+      @all_users_view_histories = ViewHistory.joins(:user)
+                                             .where.not(users: { account_status: 1 })
+                                             .order(created_at: :desc)
+                                             .limit(50)
     end
     # ログインの有無でおすすめとして参照するデータを少し変える。
 
-    @recommendations = Post.joins(:view_histories).where(view_histories: { id: @all_users_view_histories }).group("posts.id").order("COUNT(posts.id) DESC").limit(@maximum_number)
+    @recommendations = Post.joins(:user)
+                           .joins(:view_histories)
+                           .where(view_histories: { id: @all_users_view_histories })
+                           .where.not(users: { account_status: 1 })
+                           .group("posts.id")
+                           .order("COUNT(posts.id) DESC")
+                           .limit(@maximum_number)
     # この変数はおすすめ表示で使用。閲覧履歴を参照してより多く閲覧されてるものを、タグ検索のヒット数が一定以下の場合表示する。
   end
 
@@ -78,7 +103,10 @@ class SearchsController < ApplicationController
   end
 
   def autocomplete
-    @posts = Post.where("title like ?", "%#{params[:q]}%").limit(10)
+    @posts = Post.joins(:user)
+                 .where("title like ?", "%#{params[:q]}%")
+                 .where.not(users: { account_status: 1 })
+                 .limit(10)
     render partial: "autocomplete"
   end
 end
