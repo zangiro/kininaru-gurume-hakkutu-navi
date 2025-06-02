e1d9425583371871948023d4062f35cec4b3eee0
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-06-02 20:40:35 +0900

    タグ一覧のコントローラー修正

--- app/models/outher_tag.rb
+++ app/models/outher_tag.rb
@@ -2,6 +2,8 @@ class OutherTag < ApplicationRecord
   has_many :post_outher_tags, dependent: :destroy
   has_many :posts, through: :post_outher_tags
 
+  scope :active_users, -> { joins(posts: :user).where.not(users: { account_status: ACCOUNT_STATUS_INACTIVE }) }
+
   def self.ransackable_attributes(auth_object = nil)
     [ "name" ]
   end
