a61068f5964c42490049802867458924b68421ce
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-06-10 20:50:31 +0900

    decorator、投稿フォームに導入

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -2,30 +2,30 @@ class PostDecorator < Draper::Decorator
   delegate_all
 
   def resized_main_image_small
-    main_image.variant(resize_to_fill: [200, 200]).processed
+    main_image.variant(resize_to_fill: [ 200, 200 ]).processed
   end
 
   def resized_main_image_large
-    main_image.variant(resize_to_fit: [600, 600]).processed
+    main_image.variant(resize_to_fit: [ 600, 600 ]).processed
   end
 
   def resized_sub_image_first_small
-    sub_image_first.variant(resize_to_fill: [200, 200]).processed
+    sub_image_first.variant(resize_to_fill: [ 200, 200 ]).processed
   end
 
   def resized_sub_image_first_large
-    sub_image_first.variant(resize_to_fit: [400, 400]).processed
+    sub_image_first.variant(resize_to_fit: [ 400, 400 ]).processed
   end
 
   def resized_sub_image_second_small
-    sub_image_second.variant(resize_to_fill: [200, 200]).processed
+    sub_image_second.variant(resize_to_fill: [ 200, 200 ]).processed
   end
 
   def resized_sub_image_second_large
-    sub_image_second.variant(resize_to_fit: [400, 400]).processed
+    sub_image_second.variant(resize_to_fit: [ 400, 400 ]).processed
   end
 
+  #def resized_avatar
+  #  avatar.variant(resize_to_fill: [200, 200]).processed
+  #end
 end
-#  post.decorated.resized_main_image_small
-#  post.decorated.resized_sub_image_first_for_show
-#  post.decorated.resized_sub_image_second_for_show
