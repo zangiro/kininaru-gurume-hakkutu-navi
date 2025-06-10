31c5cf52ed0195a08a411ced2b5b427218871a04
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-06-10 20:34:26 +0900

    decorator、閲覧履歴、いいね一覧、トップページのおすすめに導入

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -1,7 +1,7 @@
 class PostDecorator < Draper::Decorator
   delegate_all
 
-  def resized_main_image_for_index
+  def resized_main_image_small
     main_image.variant(resize_to_fill: [200, 200]).processed
   end
 
@@ -9,7 +9,7 @@ class PostDecorator < Draper::Decorator
     main_image.variant(resize_to_fit: [600, 600]).processed
   end
 
-  def resized_sub_image_first_for_index
+  def resized_sub_image_first_small
     sub_image_first.variant(resize_to_fill: [200, 200]).processed
   end
 
@@ -17,7 +17,7 @@ class PostDecorator < Draper::Decorator
     sub_image_first.variant(resize_to_fit: [400, 400]).processed
   end
 
-  def resized_sub_image_second_for_index
+  def resized_sub_image_second_small
     sub_image_second.variant(resize_to_fill: [200, 200]).processed
   end
 
@@ -26,6 +26,6 @@ class PostDecorator < Draper::Decorator
   end
 
 end
-#  post.decorated.resized_main_image_for_show
+#  post.decorated.resized_main_image_small
 #  post.decorated.resized_sub_image_first_for_show
 #  post.decorated.resized_sub_image_second_for_show
