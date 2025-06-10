e5e3de16755424e85494e3e79a0678f145bd6cb3
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-06-10 20:29:33 +0900

    decoratorメソッド命名変更

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -5,7 +5,7 @@ class PostDecorator < Draper::Decorator
     main_image.variant(resize_to_fill: [200, 200]).processed
   end
 
-  def resized_main_image_for_show
+  def resized_main_image_large
     main_image.variant(resize_to_fit: [600, 600]).processed
   end
 
@@ -13,7 +13,7 @@ class PostDecorator < Draper::Decorator
     sub_image_first.variant(resize_to_fill: [200, 200]).processed
   end
 
-  def resized_sub_image_first_for_show
+  def resized_sub_image_first_large
     sub_image_first.variant(resize_to_fit: [400, 400]).processed
   end
 
@@ -21,7 +21,7 @@ class PostDecorator < Draper::Decorator
     sub_image_second.variant(resize_to_fill: [200, 200]).processed
   end
 
-  def resized_sub_image_second_for_show
+  def resized_sub_image_second_large
     sub_image_second.variant(resize_to_fit: [400, 400]).processed
   end
 
